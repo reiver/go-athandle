d22959b1157fe017678413f104d8652ca2e06db9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-06 07:32:13 -0700

    example

---
+++ normalize_example_test.go
@@ -0,0 +1,20 @@
+package athandle_test
+
+import (
+	"fmt"
+
+	"github.com/reiver/go-athandle"
+)
+
+func ExampleNormalize() {
+
+	var handle string = "Reiver.BSky.Social"
+
+	normalized := athandle.Normalize(handle)
+
+	fmt.Printf("handle:            %s\n", handle)
+	fmt.Printf("normalized handle: %s\n", normalized)
+
+	// Output:
+	// sdsds
+}
