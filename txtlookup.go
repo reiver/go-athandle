f205a7216b4297d55895ed13c43912cf5363a038
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-06 07:27:06 -0700

    athandle.TXTLookup()

---
+++ txtlookup.go
@@ -0,0 +1,53 @@
+package athandle
+
+import (
+	"fmt"
+	"net"
+)
+
+// TXTLookup returns any DIDs registered for the domain-name.
+//
+// Note that, internally TXTLookup calls [TXTSubDomain].
+// You should not call [TXTSubDomain] yourself, if you are calling TXTLookup.
+//
+// For example:
+//
+//	dids, err := athandle.TXTLookup("example.com")
+func TXTLookup(domainname string) ([]string, error) {
+
+	var txtdomainname string = TXTSubDomain(domainname)
+
+	// Return all the DNS TXT records for the "_atproto" sub-domain.
+	//
+	// Note that these could contain records that are NOT DIDs.
+	// For example:
+	//
+	//	did=did:plc:m2jwplpernhxkzbo4ev5ljwj
+	//	once=apple
+	//	twice=banana
+	//	thrice=cherry
+	//	fource=date
+	//	did=did:something:abcde12345
+	//
+	// Later we will filter out the non-DIDs.
+	var txtresponses []string
+	{
+		var err error
+
+		txtresponses, err = net.LookupTXT(txtdomainname)
+		if nil != err {
+			err = lookupTXTError(err, txtdomainname)
+			return nil, err
+		}
+	}
+
+	var dids []string = filterDIDs(txtresponses)
+	if len(dids) < 1 {
+		return nil, DIDTXTNotFoundError{
+			txtdomainname:txtdomainname,
+			err:fmt.Errorf("athandle: no \"did=\" DNS TXT records for %q", txtdomainname),
+		}
+	}
+
+	return dids, nil
+}
